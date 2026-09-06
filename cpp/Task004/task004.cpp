93c9119260f7593f7c2e53c5c4ce1d5393071369
Michael S. Kataev <lpestlname@gmail.com>
2026-09-07 00:20:40 +0300

    Added task004 solution

--- cpp/Task004/task004.cpp
+++ cpp/Task004/task004.cpp
@@ -5,7 +5,27 @@ int search(const std::vector<int>& nums, int target) {
     int left = 0;
     int right = static_cast<int>(nums.size()) - 1;
 
-    // TODO: реализуйте модифицированный бинарный поиск
+    while (left <= right) {
+        if (nums[left] == target)
+            return left;
+        if (nums[right] == target)
+            return right;
+        int mid = (left + right) / 2;
+        if (nums[mid] == target)
+            return mid;
+
+        if (target < nums[mid])
+            if (target < nums[left])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        else if (target >= nums[mid])
+            if (target >= nums[right])
+                right = mid - 1;
+            else
+                left = mid + 1;
+
+    }
 
     return -1;
 }
