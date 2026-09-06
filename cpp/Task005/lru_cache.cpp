e36a8400ee47d4efcd99b64db006539c4b1dcfc6
Michael S. Kataev <lpestlname@gmail.com>
2026-09-07 01:10:15 +0300

    Added task005 solution

--- cpp/Task005/lru_cache.cpp
+++ cpp/Task005/lru_cache.cpp
@@ -3,10 +3,29 @@
 LRUCache::LRUCache(int capacity) : capacity_(capacity) {}
 
 int LRUCache::get(int key) {
-    // TODO: вернуть значение или -1; обновить порядок использования
+    auto it = lookup_.find(key);
+    if (it != lookup_.end()) {
+        items_.splice(items_.begin(), items_, it->second);
+        return items_.front().second;
+    }
     return -1;
 }
 
 void LRUCache::put(int key, int value) {
-    // TODO: вставить/обновить значение; вытеснить LRU при переполнении
+    //items_.push_front({key, value});
+    auto it = lookup_.find(key);
+    if (it == lookup_.end()) {
+        if (lookup_.size() == capacity_) {
+            lookup_.erase(items_.back().first);
+            items_.pop_back();
+        }
+        items_.push_front({key, value});
+        auto it = items_.begin();
+        lookup_.emplace(key, it);
+    }
+    else {
+        items_.splice(items_.begin(), items_, it->second);
+        items_.front().second = value;
+        lookup_[key] = items_.begin();
+    }
 }
